5f7e91f324d926f4ff8ec007ae94246be6af4de0
Jhonata Fernandes <jhfernandes.oliveira@gmail.com>
2020-04-06 18:48:52 -0300

    Adicionar arquivos de projeto.

---
+++ shop-balta/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace shop_balta.Controllers
+{
+    //https://localhost:44342
+    
+    [Route("categories")]
+    public class CategoryController : ControllerBase
+    {
+        [Route("")]
+        public string MyMethod()
+        {
+            return "hello mundo!!";
+        }
+    }
+}
